78df5090c52cac3ea641b1d349ef50c026a9ccd0
ayaelhenawy <elhenawyaya21@gmail.com>
2025-09-23 17:20:51 +0300

    authentication

--- middlewares/checkExist.js
+++ middlewares/checkExist.js
@@ -1,10 +1,11 @@
 import { userModel } from "../models/user.model.js"
+import ApiError from "../utils/apiError.js"
 
 
 const checkEmail= async(req,res,next)=>{
     
     let isExist = await userModel.findOne({email:req.body.email})
-    if(isExist) return next(new AppError("email already exists.",409))
+    if(isExist) return next(new ApiError("email already exists.",409))
     next()
 }
 export{
